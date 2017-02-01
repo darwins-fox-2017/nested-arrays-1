dc2591ad197c9fc664dd9ccc8b8b17fca6f05bc5
hepiska <ego1403@gmail.com>
2017-02-01 18:48:22 +0700

    nested array 1 done

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,5 +1,18 @@
 let spreadsheet = [
   ['Number', 'Name', 'Position', 'Points per Game']
 ]
-
-// ... write your code here
+var data=[['12','Joe Schmo','Center',[14,32,7,0,23]],
+          ['9','Ken Bucket','Point Guard',[19,0,11,22,0]],
+          ['31','Harvey Key','Shooting Guard',[0,30,16,0,25]],
+          ['18','Sally Talls','Power Forward',[18,29,26,31,19]],
+          ['22','Mo DiBoux','Small Forward',[11,0,23,17,0]]
+         ]
+function addData(data) {
+  for (var i = 0; i < data.length; i++) {
+    spreadsheet.push(data[i]);
+  }
+}
+addData(data);
+console.log(spreadsheet[3][2]=="Shooting Guard");
+console.log(JSON.stringify(spreadsheet[1][3]) == JSON.stringify([14,32,7,0,23]));
+//console.log(JSON.stringify([14,32,7,0,23]));
