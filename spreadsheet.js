0378be37104d810c97be7d59e460c0501bf07108
Diky Arga <dikyarga.id@gmail.com>
2017-02-01 16:58:43 +0700

    version 1 released

--- spreadsheet.js
+++ spreadsheet.js
@@ -2,14 +2,22 @@
 let spreadsheet = [
   ['Number', 'Name', 'Position', 'Points per Game'],
   ['12', 'Joe Schm', 'Center', [14,32,7,0,23]],
+  ['32', 'Diga', 'Center Back', [56,9,2,3,10]],
+  ['17', 'Gara', 'Goal Keeper', [4,66,43,1,2]],
+  ['9', 'Angga', 'Forward', [7,54,2,0,3]],
 ]
 
 function addData(data) {
-  collections.push(data)
+  spreadsheet.push(data)
 }
 
-let data = ['1', 'Diky Arga', 'Programmer', ['9', '10', '10', '11']]
+let data = ['98', 'Diky Arga', 'Programmer', [9, 10, 10, 11]]
 
 addData(data)
 console.log('------ Collections --------');
-console.log(collections);
+console.log('----- Print All Data --------');
+console.log(spreadsheet);
+console.log('print data spreadsheet[2][2]', spreadsheet[2][2]);
+console.log(spreadsheet[2][2] == 'Center Back');
+
+console.log(JSON.stringify(spreadsheet[5][3]) == JSON.stringify([9, 10, 10, 11]));
