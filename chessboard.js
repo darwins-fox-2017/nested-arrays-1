6901db8e5651f22fd249de66979318998bc7d4e2
samuelphsinaga <samuelphsinaga@gmail.com>
2017-02-01 16:56:25 +0700

    belum selesai

--- chessboard.js
+++ chessboard.js
@@ -1,15 +1,22 @@
 'use strict'
 
-const makeChessboard = () => {
-  let chessboard = []
+var chessboard = [];
 
-  // ... write your code here
+var catur=['Benteng','Kuda','Menteri','Raja','Ratu','Menteri','Kuda','Benteng']
 
-  return chessboard
-}
+for(var i=0; i<8;i++){
+      var papan= [ ];
+      chessboard.push(papan);
+        for(var j=0; j<catur.length;j++){
+          if(i===0){
+            chessboard[i].push(catur[j]+ " hitam");
+          } else{
+            chessboard[i].push(" ");
+            }
+          }
+        }
 
-const printBoard = x => {
-  // ... write your code here
-}
 
-printBoard(makeChessboard())
+
+// console.log(papan);
+console.log(chessboard);
