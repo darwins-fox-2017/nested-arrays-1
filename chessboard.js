b248587f90fdf6e370ed5460029bca6a0f9e5017
pisanggoreng <7ofpentacles@gmail.com>
2017-02-01 22:59:32 +0700

    clear clear

--- chessboard.js
+++ chessboard.js
@@ -1,15 +1,51 @@
 'use strict'
 
-const makeChessboard = () => {
-  let chessboard = []
+const makeChessboard = (koordinat1,koordinat2) => {
+  let chessboard = [];
+  let karakter_putih = ["Benteng", "kuda", "mentri", "ratu", "raja", "mentri", "kuda", "benteng"];
+  let karakter_hitam = karakter_putih.slice().reverse();
+  let pion_putih = "Pion putih";
+  let pion_hitam = "Pion hitam";
+  let area_bebas = `Area bebas, anda berada di koordinat X = ${koordinat1} Y = ${koordinat2}`;
 
-  // ... write your code here
+  // area karakter hitam
+  // isi karakter ke dalam chessboard[0]
+  chessboard.push(karakter_putih);
 
-  return chessboard
+  // area pion hitam
+  chessboard.push([]); // chessboard[1]
+  // isi pion putih ke dalam chessboard[1]
+  for (var i = 0; i < 8; i++) {
+    chessboard[1].push(pion_putih);
+  }
+
+  // area bebas
+  for (var x = 1; x <= 4; x++) {
+    chessboard.push([]);
+  }
+
+  for (var j = 0; j < 8; j++) {
+    chessboard[2].push(area_bebas);
+    chessboard[3].push(area_bebas);
+    chessboard[4].push(area_bebas);
+    chessboard[5].push(area_bebas);
+  }
+
+  // area pion hitam
+  chessboard.push([]); // chessboard[1]
+  // isi pion putih ke dalam chessboard[1]
+  for (var i = 0; i < 8; i++) {
+  chessboard[6].push(pion_hitam);
+  }
+
+  // area karakter hitam
+  chessboard.push(karakter_hitam);
+
+  return chessboard;
 }
 
 const printBoard = x => {
-  // ... write your code here
+  console.log(x[0][4]); // raja
 }
 
 printBoard(makeChessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,5 +1,23 @@
-let spreadsheet = [
-  ['Number', 'Name', 'Position', 'Points per Game']
-]
+//write your code here
+var table = [],
+    judul = ["Number", "Name", "Position", "Point per Game"];
+    data = [
+      [12, "Joe Schmo",   "Center",        [14, 32, 7, 0, 23]   ],
+      [12, "Joe Schmo",   "Center",        [14, 32, 7, 0, 23]   ],
+      [9,  "Ken Buckets", "Point Guard",   [19, 0, 11, 12, 0]   ],
+      [31, "Harvey Kay",  "Shooting Guard",[0, 30, 16, 0, 25]   ],
+      [18, "Sally Talls", "Power Forward", [18, 29, 26, 31, 19] ],
+      [22, "Mo Diboux",   "Small Forward", [11, 0, 23, 17, 0]   ]
+    ];
 
-// ... write your code here
+function addData(data) {
+  table.push(data);
+}
+addData(judul);
+//isi data ke dalam table
+for ( var index = 0; index <= data.length-1; index++) {
+  addData(data[index]);
+}
+
+console.log(table[4][2] == "Shooting Guard");
+console.log(JSON.stringify(table[1][3]) == JSON.stringify( [14, 32, 7, 0, 23] ));
