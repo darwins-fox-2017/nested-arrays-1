7e0c470988171a2eb360e5d2c97d4b17b6f1bc28
adamsaparudin <adamsaparudin@gmail.com>
2017-02-01 16:00:28 +0700

    make it faste beibeh

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,5 +1,23 @@
+'use strict'
 let spreadsheet = [
   ['Number', 'Name', 'Position', 'Points per Game']
 ]
 
 // ... write your code here
+let addData = (data, arr) => {
+  arr.push(data)
+  return arr
+}
+
+let data = ["12", "Joe", "Director 1", [10, 10, 10, 10]]
+addData(data, spreadsheet)
+let data1 = ["9", "Ken", "Director 2", [11, 10, 10, 10]]
+addData(data1, spreadsheet)
+let data2 = ["31", "Harvey", "Director 3", [12, 10, 10, 10]]
+addData(data2, spreadsheet)
+let data3 = ["18", "Sally", "Director 4", [13, 10, 10, 10]]
+addData(data3, spreadsheet)
+let data4 = ["22", "Mo", "Director 5", [14, 10, 10, 10]]
+addData(data4, spreadsheet)
+
+console.log(spreadsheet)
