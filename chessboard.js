1aff97a6ab2ce2c32f2ca6b14a46e80a4e20fb28
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-02-02 10:17:25 +0700

    Nested array 1 Danang

--- chessboard.js
+++ chessboard.js
@@ -2,14 +2,29 @@
 
 const makeChessboard = () => {
   let chessboard = []
-
+  let anakCatur = ['Benteng', 'Kuda', 'Gajah', 'Menteri', 'Raja', 'Gajah', 'Kuda', 'Benteng'];
   // ... write your code here
+  for(let i=0;i<8;i++) {
+      chessboard[i] = []
+    for(let j=0;j<8;j++) {
+      switch(i) {
+        case 0:
+          chessboard[i][j] = anakCatur[j]+' Hitam'
+          break;
+        case 1: 
+          chessboard[i][j] = "Pion Hitam"
+        case 6: 
+          chessboard[i][j] = "Pion Putih"
+          break;
+        case 7:
+          chessboard[i][j] = anakCatur[j]+' Putih'
+          break;
+      }
+    }
+  }
 
   return chessboard
 }
 
-const printBoard = x => {
-  // ... write your code here
-}
 
-printBoard(makeChessboard())
+console.log(makeChessboard()[7][0])
