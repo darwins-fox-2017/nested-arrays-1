dcfff875d2e844c1b564577fe444824d65e92100
irwinpratajaya <irwinpratajaya@gmail.com>
2017-02-03 10:18:04 +0700

    done

--- spreadsheet.js
+++ spreadsheet.js
@@ -3,3 +3,28 @@ let spreadsheet = [
 ]
 
 // ... write your code here
+let data1 = ["12", "Joe Schmo", "Center", [14,32,7,0,23]];
+let data2 = ["9", "Ken Buckets", "Point Guard", [19,0,11,22,0]];
+let data3 = ["31", "Harvey Kay", "Shooting Guard", [0,30,16,0,25]];
+let data4 = ["18", "Sally Talls", "Power Forward", [18,29,26,31,19]];
+let data5 = ["22", "Mo DiBoux", "Small Forward", [11,0,23,17,0]];
+let table = []
+
+function addData(data) {
+	table.push(data)
+}
+
+addData(spreadsheet)
+addData(data1);
+addData(data2);
+addData(data3);
+addData(data4);
+addData(data5)
+
+// let data = ["1", "John Doe", "Director", [10,10,10,10]];
+
+console.log(table[3][2] == "Shooting Guard");
+// true
+
+console.log(JSON.stringify(table[1][3]) == JSON.stringify([14, 32, 7, 0, 23]));
+// true
