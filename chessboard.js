75a670d9489cc5f0c4f24d54058ebbdfd2ce042c
mfpebrizal <ftm.pebrizal@gmail.com>
2017-02-03 09:38:33 +0700

    Done all release

--- chessboard.js
+++ chessboard.js
@@ -1,15 +1,40 @@
 'use strict'
 
+
+
 const makeChessboard = () => {
   let chessboard = []
+  let bidak = ['Benteng','Kuda','Gajah','Mentri','Raja','Gajah', 'Kuda', 'Benteng']
+  let warna = ['Hitam', 'Putih']
 
-  // ... write your code here
+  for(let i=0; i<8; i++){
+    chessboard[i] = []
+    for(let j=0; j<8; j++){
+      if(i == 0 || i == 1){
+        if(i == 0){
+          chessboard[i].push(`${bidak[j]} ${warna[0]}`)
+        }else {
+          chessboard[i].push(`Pion ${warna[0]}`)
+        }
+      }else if(i == 6 || i == 7){
+        if(i == 7){
+          chessboard[i].push(`${bidak[j]} ${warna[1]}`)
+        }else {
+          chessboard[i].push(`Pion ${warna[1]}`)
+        }
+      }else {chessboard[i].push(" ")}
+    }
+  }
 
   return chessboard
 }
 
-const printBoard = x => {
+
+// console.log(makeChessboard());
+const printBoard = chessboard => {
+  console.log(chessboard[7][0] == "Benteng Putih")
   // ... write your code here
+  console.log(chessboard);
 }
 
 printBoard(makeChessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,5 +1,29 @@
+
 let spreadsheet = [
   ['Number', 'Name', 'Position', 'Points per Game']
 ]
 
+let data = [["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
+["9", "Ms. Buckets ", "Point Guard", [19, 0, 11, 22, 0]],
+["31", "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]],
+["7", "Sally Talls", "Power Forward ", [18, 29, 26, 31, 19]],
+["22", "MK DiBoux ", "Small Forward ", [11, 0, 23, 17, 0]]
+]
+
+
 // ... write your code here
+function addData(data,database = spreadsheet) {
+
+  for(let i=0; i<data.length; i++){
+    database.push(data[i])
+  }
+
+  return database
+}
+
+console.log(addData(data))
+
+console.log(spreadsheet[3][2] == "Shooting Guard");
+//true
+console.log(JSON.stringify(spreadsheet[1][3]) == JSON.stringify([14, 32, 7, 0, 23]))
+//true
