28df641c7236c8d7de88cbe44104a8c578e954e4
triyanarief <triyanarief@icloud.com>
2017-02-05 10:34:53 +0700

    done

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,5 +1,14 @@
-let spreadsheet = [
-  ['Number', 'Name', 'Position', 'Points per Game']
-]
+//write your code here
+var table = [];
+table[1] = ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]];
+table[1] = ["9", "Ken Buckets", "Point Guard", [14, 32, 7, 0, 23]];
+table[1] = ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]];
+table[1] = ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]];
 
-// ... write your code here
+function addData(newdata) {
+	table.push(newdata);
+}
+
+let data = ["1", "Jhon Doe", "Director", [10,10,10,10]];
+addData(data);
+console.log (table[1][2]);
