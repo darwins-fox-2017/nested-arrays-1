fd67da4d00a84c361d82585967bafc0b7b01acb6
Triyan Arief Wibowo <triyanarief@icloud.com>
2017-02-06 17:00:26 +0700

    updates spreadsheet

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,14 +1,16 @@
 //write your code here
 var table = [];
-table[1] = ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]];
-table[1] = ["9", "Ken Buckets", "Point Guard", [14, 32, 7, 0, 23]];
-table[1] = ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]];
-table[1] = ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]];
 
 function addData(newdata) {
 	table.push(newdata);
 }
 
-let data = ["1", "Jhon Doe", "Director", [10,10,10,10]];
+let data = [
+["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
+["9", "Ken Buckets", "Point Guard", [14, 32, 7, 0, 23]],
+["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
+["1", "Jhon Doe", "Director", [10,10,10,10]]
+]
+
 addData(data);
-console.log (table[1][2]);
+console.log (table);
